50a3c3cc4fd2441632a6ad0f12e049fc7258bd81
Ulia555 <juliakoposova1@gmail.com>
2025-06-10 23:36:24 +0500

    сделала задание 2-10 (доп исправила знак)

--- hw2.js
+++ hw2.js
@@ -15,13 +15,13 @@ alert  (iPhone);
 
 //задание 3
 
-const creatorJS = Brendan Eich;
-alert (creatorJS);
+const creatorJS = `Brendan Eich`;
+alert  (creatorJS);
 
 
 //задание 4
 
-let biba = 10, 
+let biba = 10;
 let boba = 2;
 
 alert (biba + boba);
@@ -29,3 +29,43 @@ alert (biba - boba);
 alert (biba * boba);
 alert (biba / boba);
 
+//задание 5
+
+let b = 2;
+alert (b ** 5);
+
+//задание 6
+
+let aa = 9;
+let bb = 2;
+
+alert (aa % bb);
+
+//задание 7
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num += 1;
+num -= 1;
+alert(num);
+
+//задание 8
+
+let age = prompt ('Сколько вам лет?');
+alert (age);
+
+//задание 9
+
+let user = {
+    name: 'Юлия',
+    age: 25,
+    isAdmin: true,
+}
+
+//задание 10
+
+let nameNAme = prompt ('Привет, ИМЯ!');
+alert (`Привет, ${nameNAme}!`);
