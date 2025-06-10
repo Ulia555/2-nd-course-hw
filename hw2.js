014760bd49b62897a98b6543b0e429b93f840aba
Ulia555 <juliakoposova1@gmail.com>
2025-06-10 23:35:10 +0500

    сделала задание 2-10

--- hw2.js
+++ hw2.js
@@ -1,5 +1,31 @@
+//задание 1
+
 let a = 10;
 alert (a);
 
 a = 20;
 alert (a);
+
+
+//задание 2
+
+const iPhone = 2007;
+alert  (iPhone);
+
+
+//задание 3
+
+const creatorJS = Brendan Eich;
+alert (creatorJS);
+
+
+//задание 4
+
+let biba = 10, 
+let boba = 2;
+
+alert (biba + boba);
+alert (biba - boba);
+alert (biba * boba);
+alert (biba / boba);
+
