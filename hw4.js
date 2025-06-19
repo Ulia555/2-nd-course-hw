b9f14d3f044cdde533db85ba9b4b0eeb7f16a140
Ulia555 <juliakoposova1@gmail.com>
2025-06-19 09:29:31 +0500

    код раскомментирован

--- hw4.js
+++ hw4.js
@@ -1,46 +1,46 @@
 // // задание 1
 
-// for (let i = 0; i < 2; i++) {
-//     console.log('Привет!');
-// }
+for (let i = 0; i < 2; i++) {
+    console.log('Привет!');
+}
 
 // // задание 2
 
-// for (let i = 0; i <= 5; i++) {
-//     console.log(i);
+for (let i = 0; i <= 5; i++) {
+    console.log(i);
     
-// }
+}
 
 
 // // задание 3
 
-// for (let b = 7;b <= 22; b++) {
-//     console.log(b);
-// }
+for (let b = 7;b <= 22; b++) {
+    console.log(b);
+}
 
 // // задание 4
 
-// const obj = {
-//    kolya: '200',
-//     vasya: '300',
-//     petya: '400'
-// }
+const obj = {
+   kolya: '200',
+    vasya: '300',
+    petya: '400'
+}
 
-// for (let key in obj) {
-//    console.log(`${key}: ${obj[key]}`);
-// }
+for (let key in obj) {
+   console.log(`${key}: ${obj[key]}`);
+}
 
 // // задание 5
 
-// let n = 1000;
-// let num = 0;
+let n = 1000;
+let num = 0;
 
-// while (n >= 50) {
-//     n /= 2;
-//     console.log(n);
-//     num++;
-// }
-// console.log(`проходов цикла: ${num}`);
+while (n >= 50) {
+    n /= 2;
+    console.log(n);
+    num++;
+}
+console.log(`проходов цикла: ${num}`);
 
 
 // // задание 6
